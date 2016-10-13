066cb2d0809f65d3be940c9b6f32aeab09664c93
Ronald Tol <tol.ronald@gmail.com>
2016-10-13 19:09:47 +0200

    Begin van de navigatie voor tijdlijn

--- Website/assets/js/main.js
+++ Website/assets/js/main.js
@@ -2,6 +2,23 @@ $(document).ready(function() {
     // Scroll weg van homepage direct naar tijdlijn
     $('#fullpage').fullpage({
         scrollOverflow: true,
-        normalScrollElements: '#tijdlijnSection'
+        normalScrollElements: '#tijdlijnSection',
+
+        //voor de bovennav die tevoorschijn komt bij de tweede sectie
+        onLeave: function(index, nextIndex, direction){
+            var leavingSection = $(this);
+
+            //after leaving section 1
+            if(index == 1 && direction =='down'){
+                $( "#topNav" ).animate({
+                    top: "0"
+                }, 600 );
+            }
+            else if(index == 2 && direction == 'up'){
+                $( "#topNav" ).animate({
+                    top: "-45px"
+                }, 600 );
+            }
+        }
     });
 });
