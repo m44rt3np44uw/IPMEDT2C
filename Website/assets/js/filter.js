d78090f5fbb882bdaea3d0b3c09f95170f61df44
Maarten Paauw <maartenpaauw@gmail.com>
2016-11-02 16:08:53 +0100

    Filter werkt nu

---
+++ Website/assets/js/filter.js
@@ -0,0 +1,24 @@
+// Als de tijdlijn geladen is.
+$(document).on('timeline-ready', function() {
+
+    // Als de filter geklikt wordt.
+    $('.filter').on('click', function() {
+
+        // Haal alle items op.
+        var $items = $('.cd-timeline-block');
+
+        // Loop door alle items heen.
+        $.each($items, function(index, item) {
+
+            if($(item).is(':visible')) {
+                if($(item).hasClass('filter-trump') || $(item).hasClass('filter-clinton')) {
+                    $(item).fadeOut();
+                }
+            } else {
+                if($(item).hasClass('filter-trump') || $(item).hasClass('filter-clinton')) {
+                    $(item).fadeIn();
+                }
+            }
+        });
+    });
+});
